50ee450df0d9894cbd1468c8d15534e5d53e7881
Karleigh <kedehlsen@gmail.com>
2019-10-28 10:02:00 -0700

    notes'

--- countyDemographicsUnfinished.py
+++ countyDemographicsUnfinished.py
@@ -12,20 +12,38 @@ def main():
 
 def high_income_counties(counties):
     """Return a LIST of the counties with a median household income over $90,000."""
-    
+    list = []
+    for data in counties:
+        if data['Income']['Median Household Income'] > 90000:
+            list.append(data['County'])
+    print(list)
 
 def lowest_median_income(counties):
     """Return a name of a county with the lowest median household income"""
-    
+    min = counties[0]['Income']['Median Household Income']
+    county = counties[0]['County']
+    for data in counties:
+        if data['Income']['Median Household Income'] < min:
+            min = data['Income']['Median Household Income']
+            county = data['County']
+    print(county)
 
 def alphabetically_first_county(counties):
     """Return the county with the name that comes first alphabetically."""
     #Hint: you can use < to compare strings in Python. ex) "cat" < "dog" gives the value True
-
+    county = counties[0]['County']
+    for data in counties:
+        if data['County'] < county:
+            county = data['County']
+    print(county)
     
 def percent_most_under_18(counties):
     """Return the highest percent of under 18 year olds."""    
-    
+    percent = counties[0]['Age']['Percent Under 18 Years']
+    for data in counties:
+        if data['County'] < county:
+            county = data['County']
+    print(county)
 
 def county_most_under_18(counties):
     """Return the name a county with the highest percent of under 18 year olds."""
